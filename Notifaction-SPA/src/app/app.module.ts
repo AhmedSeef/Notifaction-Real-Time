d0414cf8c425a8cbf9ab888acbe11dd274811422
ahmed.seif <ahmed.seef.gooda@gmail.com>
2019-12-31 16:15:55 +0200

    Add signalR in Angular

--- Notifaction-SPA/src/app/app.module.ts
+++ Notifaction-SPA/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
 
 import { AppComponent } from './app.component';
 
