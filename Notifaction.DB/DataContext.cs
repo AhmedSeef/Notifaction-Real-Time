d15dcd370446ce17460055cd00b1a36d1be6b457
ahmed.seif <ahmed.seef.gooda@gmail.com>
2019-12-31 13:39:20 +0200

    craete data base lasyer, and create database

---
+++ Notifaction.DB/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notifaction.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notifaction.DB
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<patient> patient { get; set; }
+        public DbSet<Doctor> doctor { get; set; }
+        public DbSet<Order> order { get; set; }
+        public DbSet<Notification> notification { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
